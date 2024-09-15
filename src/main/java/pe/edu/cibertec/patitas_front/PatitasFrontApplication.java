8fdb947f3ecf04835d64cb845336f2e10f3bf1a5
camilavr127 <dhayanavr@gmail.com>
2024-09-15 16:15:07 -0500

    Carga inicial del proyecto

---
+++ src/main/java/pe/edu/cibertec/patitas_front/PatitasFrontApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/patitas_front/PatitasFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitasFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
